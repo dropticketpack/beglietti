45ed28e5508e79fd8345a40c0eab76218f2be222
dropticketpack <mohammadasadi04@outlook.com>
2026-02-20 19:18:38 +0100

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,34 +1,43 @@
 let left = 0
 let left2 = 0
 let left3 = 0
-window.setInterval(function (){
-    if(left%2 == 0){
-        document.getElementsByClassName("ticket_green_box1")[0].style.transition = 'left 1000ms'
-        document.getElementsByClassName("ticket_green_box1")[0].style.left = '200%'
-    }else{
-        document.getElementsByClassName("ticket_green_box1")[0].style.transition = 'left 0ms'
-        document.getElementsByClassName("ticket_green_box1")[0].style.left = '-200%'
-    }
-    left++
-} , 800)
-window.setInterval(function (){
-    if(left2%2 == 0){
-        document.getElementsByClassName("ticket_green_box2")[0].style.transition = 'left 1000ms'
-        document.getElementsByClassName("ticket_green_box2")[0].style.left = '200%'
-    }else{
-        document.getElementsByClassName("ticket_green_box2")[0].style.transition = 'left 0ms'
-        document.getElementsByClassName("ticket_green_box2")[0].style.left = '-200%'
-    }
-    left2++
-} , 800)
+const greenBox1El = document.getElementsByClassName("ticket_green_box1")[0]
+const greenBox2El = document.getElementsByClassName("ticket_green_box2")[0]
+const greenBox3El = document.getElementsByClassName("ticket_green_box3")[0]
 
-window.setInterval(function (){
-    if(left3%2 == 0){
-        document.getElementsByClassName("ticket_green_box3")[0].style.transition = 'left 1000ms'
-        document.getElementsByClassName("ticket_green_box3")[0].style.left = '200%'
-    }else{
-        document.getElementsByClassName("ticket_green_box3")[0].style.transition = 'left 0ms'
-        document.getElementsByClassName("ticket_green_box3")[0].style.left = '-200%'
-    }
-    left3++
-} , 800)
+if (greenBox1El) {
+    window.setInterval(function (){
+        if(left%2 == 0){
+            greenBox1El.style.transition = 'left 1000ms'
+            greenBox1El.style.left = '200%'
+        }else{
+            greenBox1El.style.transition = 'left 0ms'
+            greenBox1El.style.left = '-200%'
+        }
+        left++
+    } , 800)
+}
+if (greenBox2El) {
+    window.setInterval(function (){
+        if(left2%2 == 0){
+            greenBox2El.style.transition = 'left 1000ms'
+            greenBox2El.style.left = '200%'
+        }else{
+            greenBox2El.style.transition = 'left 0ms'
+            greenBox2El.style.left = '-200%'
+        }
+        left2++
+    } , 800)
+}
+if (greenBox3El) {
+    window.setInterval(function (){
+        if(left3%2 == 0){
+            greenBox3El.style.transition = 'left 1000ms'
+            greenBox3El.style.left = '200%'
+        }else{
+            greenBox3El.style.transition = 'left 0ms'
+            greenBox3El.style.left = '-200%'
+        }
+        left3++
+    } , 800)
+}
--- js/scriptEE03.js
+++ js/scriptEE03.js
@@ -1,21 +1,24 @@
 let left = 0
-window.setInterval(function (){
-    if(left%2 == 0){
-        document.getElementsByClassName("ticket_green_box")[0].style.transition = 'left 1000ms'
-        document.getElementsByClassName("ticket_green_box")[0].style.left = '200%'
-    }else{
-        document.getElementsByClassName("ticket_green_box")[0].style.transition = 'left 0ms'
-        document.getElementsByClassName("ticket_green_box")[0].style.left = '-200%'
-    }
-    left++
-} , 800)
+const greenBoxEl = document.getElementsByClassName("ticket_green_box")[0]
+if (greenBoxEl) {
+    window.setInterval(function (){
+        if(left%2 == 0){
+            greenBoxEl.style.transition = 'left 1000ms'
+            greenBoxEl.style.left = '200%'
+        }else{
+            greenBoxEl.style.transition = 'left 0ms'
+            greenBoxEl.style.left = '-200%'
+        }
+        left++
+    } , 800)
+}
 
 
 
 const d = new Date();
 let time = d.getTime();
-time -= 600000
-let expireTime = time + 2400000
+time -= 60000
+let expireTime = time + 2940000
 
 let validationTime = new Date(time)
 let ExpireTime = new Date(expireTime)
@@ -23,31 +26,40 @@ let ExpireTime = new Date(expireTime)
 
 // let dayOfValidation = validationTime.getDate().toString().length === 2 ? validationTime.getDate() : "0" +  validationTime.getDate()
 
+let dayOfValidation;
 if (validationTime.getDate().toString().length === 2)
     dayOfValidation = validationTime.getDate()
 else
     dayOfValidation = "0" +  validationTime.getDate()
 
+let MonthOfValidation;
 if ((validationTime.getMonth()+1).toString().length === 2)
     MonthOfValidation = (validationTime.getMonth()+1)
 else
     MonthOfValidation = "0" +  (validationTime.getMonth()+1)
-let yearOfValidation = validationTime.getFullYear().toString().substr(2 , 2)
+let yearOfValidation = validationTime.getFullYear().toString().substring(2 , 4)
 let HoursOfValidation = validationTime.getHours().toString().length === 2 ? validationTime.getHours() : "0" +  validationTime.getHours()
 let minutesOfValidation = validationTime.getMinutes().toString().length === 2 ? validationTime.getMinutes() : "0" +  validationTime.getMinutes()
 
 
 
 let dayOfExpire = ExpireTime.getDate().toString().length === 2 ? ExpireTime.getDate() : "0" +  ExpireTime.getDate()
-// let MonthOfExpire = ExpireTime.getMonth().toString().length === 2 ? ExpireTime.getMonth() : "0" +  ExpireTime.getDate()
-let yearOfExpire = ExpireTime.getFullYear().toString().substr(2 , 2)
+let MonthOfExpire;
+if ((ExpireTime.getMonth()+1).toString().length === 2)
+    MonthOfExpire = (ExpireTime.getMonth()+1)
+else
+    MonthOfExpire = "0" +  (ExpireTime.getMonth()+1)
+let yearOfExpire = ExpireTime.getFullYear().toString().substring(2 , 4)
 let HoursOfExpire = ExpireTime.getHours().toString().length === 2 ? ExpireTime.getHours() : "0" +  ExpireTime.getHours()
 let minutesOfExpire = ExpireTime.getMinutes().toString().length === 2 ? ExpireTime.getMinutes() : "0" +  ExpireTime.getMinutes()
 
 
 let validationData = dayOfValidation + "/" + MonthOfValidation + "/" + yearOfValidation + ", " + HoursOfValidation + ":" + minutesOfValidation
-let expireDate = dayOfExpire + "/" + MonthOfValidation + "/" + yearOfExpire + ", " + HoursOfExpire + ":" + minutesOfExpire
+let expireDate = dayOfExpire + "/" + MonthOfExpire + "/" + yearOfExpire + ", " + HoursOfExpire + ":" + minutesOfExpire
 
-document.getElementsByClassName("purchase_time")[0].innerHTML = validationData
-document.getElementsByClassName("expire_date")[0].innerHTML = expireDate
-document.getElementsByClassName("validation_date")[0].innerHTML = validationData
+const purchaseTimeEl = document.getElementsByClassName("purchase_time")[0]
+const expireDateEl = document.getElementsByClassName("expire_date")[0]
+const validationDateEl = document.getElementsByClassName("validation_date")[0]
+if (purchaseTimeEl) purchaseTimeEl.innerHTML = validationData
+if (expireDateEl) expireDateEl.innerHTML = expireDate
+if (validationDateEl) validationDateEl.innerHTML = validationData
